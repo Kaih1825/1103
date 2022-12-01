e16e5a4e4ea60ea565ccf5505fd7cede6e727b91
Kai <imatch181@gmail.com>
2022-12-01 21:05:57 +0800

    Update

--- app/src/main/java/com/extra/a1103/passport_home.kt
+++ app/src/main/java/com/extra/a1103/passport_home.kt
@@ -29,7 +29,7 @@ import kotlinx.android.synthetic.main.dialog.*
 
 
 class passport_home : AppCompatActivity() {
-    //120100902
+    //120100905
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_passport_home)
