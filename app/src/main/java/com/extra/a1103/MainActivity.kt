007fed8c602f0591e634635b3601e42547da443b
Kai <imatch181@gmail.com>
2022-12-02 17:05:19 +0800

    1202

--- app/src/main/java/com/extra/a1103/MainActivity.kt
+++ app/src/main/java/com/extra/a1103/MainActivity.kt
@@ -8,7 +8,8 @@ import android.os.Handler
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
