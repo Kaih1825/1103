5ed52cb02e1e756eb8dbe0158375d9e314c7dfd1
Kai <imatch181@gmail.com>
2022-12-07 18:50:31 +0800

    1207

--- app/src/main/java/com/extra/a1103/passport_home.kt
+++ app/src/main/java/com/extra/a1103/passport_home.kt
@@ -132,12 +132,21 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
         var sharedPreferencesEdit=sharedPreferences.edit()
         edt_cnName.hint=sharedPreferences.getString("cnName","王曉明")
         edt_enName.hint=sharedPreferences.getString("enName","Xiao Ming")
-        btn_birdate.text=sharedPreferences.getString("birDate","1996/05/18")
+        btn_birdate.text=sharedPreferences.getString("birYear","1996")+"/"+sharedPreferences.getString("birMonth","05")+"/"+sharedPreferences.getString("birDay","18")
         edt_id.hint=sharedPreferences.getString("id","J1236456789")
         edt_cardId1.hint=sharedPreferences.getString("cardId1","0000")
         edt_cardId2.hint=sharedPreferences.getString("cardId2","1234")
         edt_cardId3.hint=sharedPreferences.getString("cardId3","5678")
         edt_cardId4.hint=sharedPreferences.getString("cardId4","4321")
+        genderSwitch.isChecked=sharedPreferences.getBoolean("gender",false)
+        if(genderSwitch.isChecked){
+            txtWomen.setTextColor(activity.resources.getColor(R.color.lightBlue))
+            txtMen.setTextColor(activity.resources.getColor(R.color.lightBlueGrey))
+        }
+        else{
+            txtMen.setTextColor(activity.resources.getColor(R.color.lightBlue))
+            txtWomen.setTextColor(activity.resources.getColor(R.color.lightBlueGrey))
+        }
         close.setOnClickListener {
             activity.dialogBackground.visibility = View.INVISIBLE
             super.cancel()
@@ -148,7 +157,9 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
             .setTheme(R.style.datepicker)
             .build()
         var calender= Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        var date=btn_birdate.text
+        var year:String="1996"
+        var month:String="05"
+        var day:String="18"
         var btDayHaveChange=false
         btn_birdate.setOnClickListener{
             var activity2=activity as passport_home
@@ -156,9 +167,11 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
         }
         dialog.addOnPositiveButtonClickListener {
             calender.timeInMillis= dialog.selection!!
-            date="${calender.get(Calendar.YEAR)}/${calender.get(Calendar.MONTH)+1}/${calender.get(Calendar.DATE)}"
+            year="${calender.get(Calendar.YEAR)}"
+            month="${calender.get(Calendar.MONTH)+1}"
+            day="${calender.get(Calendar.DATE)}"
+            btn_birdate.text="${calender.get(Calendar.YEAR)}/${calender.get(Calendar.MONTH)+1}/${calender.get(Calendar.DATE)}"
             btDayHaveChange=true
-            btn_birdate.text=date
         }
         edt_cardId2.isEnabled=false
         edt_cardId3.isEnabled=false
@@ -177,6 +190,7 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
         })
+
         edt_cardId3.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
                 edt_cardId4.isEnabled = edt_cardId3.text.isNotEmpty()
@@ -184,19 +198,32 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
         })
+        genderSwitch.setOnClickListener {
+            if(genderSwitch.isChecked){
+                txtWomen.setTextColor(activity.resources.getColor(R.color.lightBlue))
+                txtMen.setTextColor(activity.resources.getColor(R.color.lightBlueGrey))
+            }
+            else{
+                txtMen.setTextColor(activity.resources.getColor(R.color.lightBlue))
+                txtWomen.setTextColor(activity.resources.getColor(R.color.lightBlueGrey))
+            }
+
+        }
         dialogBtn_close.setOnClickListener {
             activity.dialogBackground.visibility = View.INVISIBLE
             if(!edt_cnName.text.isEmpty()){
                 sharedPreferencesEdit.putString("cnName",edt_cnName.text.toString())
-                activity.txt_chName.text=edt_cnName.text.toString()
+
             }
             if(!edt_enName.text.isEmpty()){
                 sharedPreferencesEdit.putString("enName",edt_enName.text.toString())
-                activity.txt_enName.text=edt_enName.text.toString()
+
             }
             if(btDayHaveChange){
-                sharedPreferencesEdit.putString("birDate",date.toString())
-                activity.txt_birDate.text="${calender.get(Calendar.YEAR)}.${calender.get(Calendar.MONTH)+1}.${calender.get(Calendar.DATE)}"
+                sharedPreferencesEdit.putString("birYear",year)
+                sharedPreferencesEdit.putString("birMonth",month)
+                sharedPreferencesEdit.putString("birDay",day)
+
             }
             if(!edt_id.text.isEmpty()){
                 sharedPreferencesEdit.putString("id",edt_id.text.toString())
@@ -213,6 +240,12 @@ class infoDialog(context: Context,var activity: Activity):Dialog(context){
             if(edt_cardId4.text.isNotEmpty()){
                 sharedPreferencesEdit.putString("cardId4",edt_cardId4.text.toString())
             }
+            sharedPreferencesEdit.apply()
+            activity.txt_chName.text=sharedPreferences.getString("cnName","王曉明")
+            activity.txt_enName.text=sharedPreferences.getString("enName","Xiao Ming")
+            activity.txt_birDate.text=sharedPreferences.getString("birDate","1996/05/18")
+            activity.txt_birDate.text="${ sharedPreferences.getString("birYear","1996")}.${sharedPreferences.getString("birMonth","05")}.${sharedPreferences.getString("birDay","18")}"
+            sharedPreferencesEdit.putBoolean("gender",genderSwitch.isChecked)
             sharedPreferencesEdit.apply()
             super.cancel()
         }
